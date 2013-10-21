d18bba2cec016e590fdfd460fe94dd65c21cf6ec
Nicholas J <nicholas.a.johns5@gmail.com>
2013-10-21 19:00:32 +0900

    adding x86 asm

---
+++ x86_asm/hello.asm
@@ -0,0 +1,26 @@
+; Find your proper architecture with nasm -hf
+; Compile: nasm -f $arch -l hello.lst hello.asm 
+; (ex: nasm -f elf64 -l hello.lst hello.asm)
+; Link: gcc -o hello hello.o
+	SECTION .data
+msg:	db "Hello DevFest", 10
+len:	equ $-msg
+
+	SECTION .text
+	global main
+main:
+	mov edx, len
+	mov ecx, msg
+	call greet
+
+	mov ebx, 0
+	mov eax, 1
+	int 0x80
+
+greet:
+	mov ebx, 1
+	mov eax, 4
+	int 0x80
+
+	ret
+
