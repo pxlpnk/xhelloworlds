345347a5bcd06fa3c0586e1625a44037eb581ffc
Martin Natano <natano@natano.net>
2013-10-21 10:39:41 +0200

    adding python and python3

---
+++ python/hello.py
@@ -0,0 +1,10 @@
+def main():
+    greet(u'DevFest')
+
+
+def greet(name):
+    print u'Hello {0}'.format(name)
+
+
+if __name__ == '__main__':
+    main()
---
+++ python3/hello.py
@@ -0,0 +1,10 @@
+def main():
+    greet('DevFest')
+
+
+def greet(name):
+    print('Hello {0}'.format(name))
+
+
+if __name__ == '__main__':
+    main()
