5e4df7b596fcdb69e913e331fa03b57743579ea7
Martin Natano <natano@natano.net>
2013-11-04 13:44:29 +0100

    add java

---
+++ java/hello.java
@@ -0,0 +1,11 @@
+import java.lang.System;
+
+class hello {
+    private static void greet(String name) {
+        System.out.println("Hello " + name);
+    }
+
+    public static void main(String[] args) {
+        greet("DevFest");
+    }
+}
